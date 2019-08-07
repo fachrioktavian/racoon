4c77463e9776744563f5cdf2765859d5412991ee
Fachrizal Oktavian Perkasa <fachrizal.oktavian@bukalapak.com>
2019-08-07 11:13:59 +0700

    add some http method and parser

--- racoon.py
+++ racoon.py
@@ -1,4 +1,4 @@
-import re, sys, requests, threading
+import re, sys, requests, threading, argparse
 import concurrent.futures
 from agent import Agent
 
@@ -20,11 +20,6 @@ def getInfo(inp, a):
         elif (nowdata):
             a.setData(line)
         it+=1
-    '''print(a.method)
-    print(a.param)
-    print(a.headers)
-    print(a.data)
-    print(a.url)'''
 
 def sendPost(i, u, x, y, z):
     r = requests.post(u, headers=x, params=y, data=z, verify=False)
@@ -34,31 +29,48 @@ def sendPut(i, u, x, y, z):
     r = requests.put(u, headers=x, params=y, data=z, verify=False)
     print('thread-'+ str(i) + '> ' + str(r.content))
 
-def sendReq(a, n, h):
+def sendGet(i, u, x, y, z):
+    r = requests.get(u, headers=x, params=y, data=z, verify=False)
+    print('thread-'+ str(i) + '> ' + str(r.content))
+
+def sendPatch(i, u, x, y, z):
+    r = requests.patch(u, headers=x, params=y, data=z, verify=False)
+    print('thread-'+ str(i) + '> ' + str(r.content))
+
+def sendReq(a, w, h):
     u = h + '://' + a.url
     param = a.param
     data = a.data
     headers = a.headers
     if (a.method == 'POST'):
-        with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
-            for i in range (0, n):
+        with concurrent.futures.ThreadPoolExecutor(max_workers=w) as executor:
+            for i in range (0, w):
                 executor.submit(sendPost, i, u, headers, param, data)
-                #threading.Thread(target=sendPost(i, u, headers, param, data)).start()
     elif (a.method == 'PUT'):
-        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-            for i in range (0, n):
+        with concurrent.futures.ThreadPoolExecutor(max_workers=w) as executor:
+            for i in range (0, w):
                 executor.submit(sendPut, i, u, headers, param, data)
-
+    elif (a.method == 'GET'):
+        with concurrent.futures.ThreadPoolExecutor(max_workers=w) as executor:
+            for i in range (0, w):
+                executor.submit(sendGet, i, u, headers, param, data)
+    elif (a.method == 'PATCH'):
+        with concurrent.futures.ThreadPoolExecutor(max_workers=w) as executor:
+            for i in range (0, w):
+                executor.submit(sendPatch, i, u, headers, param, data)
 
 if __name__ == '__main__':
-    inp = sys.argv[1]
-    a = Agent()
-    getInfo(inp, a)
-    sendReq(a, 20, 'https')
+    parser = argparse.ArgumentParser(description='racoon - race condition automation')
+    parser.add_argument('-t', '--target', type=str, help='target file config')
+    parser.add_argument('--https', help='use https. default: no', action='store_true')
+    parser.add_argument('-w', '--worker', type=int, help='how many workers run in a time')
+    args = parser.parse_args()
 
-    '''fr = open(inp, 'r')
-    for line in fr:
-        if (line == '\n'):
-            print('newline')
-            continue
-        print(line)'''
+    if(not args.target or not args.worker):
+        parser.print_help()
+        sys.exit(1)
+
+    print('Starting '+str(args.worker)+' threads to test '+args.target+'...')
+    a = Agent()
+    getInfo(args.target, a)
+    sendReq(a, args.worker, 'https') if args.https else sendReq(a, args.worker, 'http')
